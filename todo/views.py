f9df92df8a0707828f8e7b12d93213311ee86d66
malinpalo <malinpalo.mp@gmail.com>
2023-03-01 12:31:37 +0000

    Add functions for deleting data

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import item
+from .forms import itemForm
 
 # Create your views here.
 
@@ -10,3 +11,29 @@ def get_todo_list(request):
         'items' : items
     }
     return render(request, 'todo/todo_list.html', context)
+
+
+def add_item(request):
+    if request.method == 'POST':
+        form= itemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    form = itemForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/add_item.html', context)
+
+def edit_item(request, item_id):
+    Item = get_object_or_404(item, id=item_id)
+    if request.method == 'POST':
+        form= itemForm(request.POST, instance=Item)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    form = itemForm(instance=Item)
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/edit_item.html', context)
